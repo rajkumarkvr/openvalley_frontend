6ce243ed33dedb52afbaa2350a96427374c565f2
AllinRoshya <kookierosy1@gmail.com>
2025-02-11 13:08:39 +0530

    Updated Landing Page UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,14 @@
-import { useState } from 'react'
+import { StrictMode, useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import 'bootstrap/dist/css/bootstrap.css'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from './Pages/Login/LoginPage.jsx';
-import LandingPage from './Pages/Login/LandingPage/LandingPage.jsx';
+import viteLogo from '/vite.svg';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <LandingPage />
+      <LoginPage />
+    </>
+  )
  
 }
 
--- Frontend/src/Pages/Login/LandingPage/LandingPage.jsx
+++ Frontend/src/Pages/Login/LandingPage/LandingPage.jsx
@@ -1,19 +1,39 @@
-import { useNavigate } from "react-router-dom";
+
 import "./LandingPage.css";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+
+
 function LandingPage(){
-    const navigate = useNavigate();
 
     return (
       <>
-      <div className="header"><img src="" className="logo"/>
-        <button className="btn in sign-btn" onClick={() => navigate("/login")}>Sign In</button>
-      <button className="btn sign-btn" onClick={() => navigate("/login")}>Sign Up</button></div>
+      <div className="header">
+      <div className="logoContainer">
+    <Avatar className="logo" alt="Git Grove" src="/logo.png" />
+    <h3 className="proName">Git Grove</h3>
+  </div>
+      <div className="authBtn">  <a href="/Pages/Login/LoginPage/signin">
+    <button className="btn in sign-btn">Sign In</button>
+</a>
+<a href="/Pages/Login/LoginPage/signin">
+    <button className="btn sign-btn">Sign Up</button>
+</a>
+</div>
+      </div>
           <div className="div1">
           <h1 className="slogan">Code together, ship faster,innovate smarter</h1>
           <h1 className="header1">Be part of the leading AI-driven platform empowering developers worldwide.</h1>
+          <a href="/Pages/Login/LoginPage/signin"> <div className="start">
+          <Button  sx={{   fontSize: "16px","&:focus": { outline: "none" },  "&:active": { boxShadow: "none" } 
+  }} className="getBtn" variant="contained" endIcon={<ArrowForwardIcon />}>Get Started</Button></div></a>
           </div> 
+          <img className="AiImg" src="/Ai.png" alt="AI Image"/>
       </>
     );
 }
+
 
 export default LandingPage;
