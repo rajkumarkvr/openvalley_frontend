33ec58cff39f8bc8507f1562aeb4410e1640fc7e
rajkumarkvr <rajkumar2005kvr@gmail.com>
2025-02-24 12:24:57 +0530

    request

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,8 @@
 // import 'bootstrap/dist/css/bootstrap.css'
-import LoginPage from './Pages/Login/LoginPage.jsx';
-import HomePage from './Pages/HomePage/HomePage.jsx';
-import "./App.css"
-import { Route,Routes } from 'react-router-dom';
+import LoginPage from "./Pages/Login/LoginPage.jsx";
+import HomePage from "./Pages/HomePage/HomePage.jsx";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -16,24 +16,24 @@ function App() {
         <Route exact path="/settings" element={<LoginPage />} />
       </Routes>
     </>
-  )
+  );
   list = [
-  {
-    name:"Frontend",
-    latestCommitMessage:"something",
-    timestamp:"2mins",
-  },
-  {
-    name:"Frontend",
-    latestCommitMessage:"something",
-    timestamp:"2mins",
-  },
     {
-    name:"Frontend",
-    latestCommitMessage:"something",
-    timestamp:"2mins",
-  },
- ]
+      name: "Frontend",
+      latestCommitMessage: "something",
+      timestamp: "2mins",
+    },
+    {
+      name: "Frontend",
+      latestCommitMessage: "something",
+      timestamp: "2mins",
+    },
+    {
+      name: "Frontend",
+      latestCommitMessage: "something",
+      timestamp: "2mins",
+    },
+  ];
 }
 
-export default App
+export default App;
